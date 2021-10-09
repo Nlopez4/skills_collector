6856b9a1fbd062c7c7c71be1d8af4558b86bedc3
nataliel2514 <natalielopez2514@gmail.com>
2021-10-09 09:03:15 -0500

    search bar created

--- main_app/views.py
+++ main_app/views.py
@@ -2,17 +2,30 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from .models import gym
 # Create your views here.
+
+
 class Home(TemplateView):
     template_name = "home.html"
-    
+
+
 class About(TemplateView):
     template_name = "about.html"
-    
+
 # OTHERS
+
+
 class GymList(TemplateView):
     template_name = "gym_list.html"
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["gym"] = gym.objects.all() # Here we are using the model to query the database for us.
+        name = self.request.GET.get("name")
+        if name != None:
+            context["gym"] = gym.objects.filter(name__icontains=name)
+            # We add a header context that includes the search param
+            context["header"] = f"Searching for {name}"
+        else:
+            context["gym"] = gym.objects.all()
+            # default header for not searching
+            context["header"] = "Local Gyms"
         return context
