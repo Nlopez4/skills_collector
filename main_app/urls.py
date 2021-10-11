114fa3f0716625a19d5111b453ad8b44d00c14cd
nataliel2514 <natalielopez2514@gmail.com>
2021-10-10 19:07:53 -0500

    add sign up

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
          views.GymUpdate.as_view(), name="gym_update"),
     path('gym/<int:pk>/delete',
          views.GymDelete.as_view(), name="gym_delete"),
-      path('gym/<int:pk>/skills/new/',
-         views.SkillCreate.as_view(), name="skill_create")
+     path('gym/<int:pk>/skills/new/',
+         views.SkillCreate.as_view(), name="skill_create"),
+     path('accounts/signup/', views.Signup.as_view(), name="signup"),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 # from django.urls import reverse
 from django.views import View
-from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
@@ -84,4 +85,19 @@ class SkillCreate(View):
         return redirect('gym_detail', pk=pk)
 
 
-    
+class Signup(View):
+    # show a form to fill out
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+    # on form submit validate the form and login the user.
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("gym_list")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)
